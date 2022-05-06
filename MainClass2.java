9b3ba2211a12beb9db49eda7e8db400733f8b24d
Eric Castellar <86270246+Ericcastell@users.noreply.github.com>
2022-05-06 10:55:41 -0300

    add 2st exercises

---
+++ MainClass2.java
@@ -0,0 +1,19 @@
+
+/*Faça um programa que receba como argumento de duas notas de um aluno, calcule e exiba a média, a situação do aluno e caso a média do aluno seja superior a 9.5, exiba tambéma mensagem de parabéns. A situação pode ser;
+ * Aprovado, caso a média seja igual ou maior 6;
+ * Final, caso a média seha igual ou maior a 3 e inferior a 6;
+ * Reprovado, caso a média seja menor que 3;
+ */
+
+public class MainClass2 {
+	
+	public static void main (String [] args) {
+		double nota1 = Double.parseDouble (args[0]);
+		double nota2 = Double.parseDouble (args[1]);
+		double media = (nota1 + nota2)/2;
+		
+		
+		
+	}
+	
+}

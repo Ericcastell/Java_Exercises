f97f1f6c10381f557feb2c5f6fe14d09770fa3f9
Eric Castellar <86270246+Ericcastell@users.noreply.github.com>
2022-05-06 10:57:33 -0300

    update formatting

--- MainClass.java
+++ MainClass.java
@@ -1,107 +1,106 @@
-		/* Condicionais:
-		 * 
-		 * Condicional Simples:
-		 * 
-		 * Forma geral:
-		 * 
-		 * 1) if (condicao)
-		 *     comando;
-		 *     
-		 * 2) if(condicao){
-		 * comando1;
-		 * comando2;
-		 * ...
-		 * comandoN;
-		 * }
-		 * 
-		 * Condicional Composta:
-		 * 
-		 * Forma Geral:
-		 * 
-		 * 1) if (condicao)
-		 * comando1;
-		 * else
-		 * comando2;
-		 * 
-		 * 2) if (condicao){
-		 * comando1;
-		 * comando2;
-		 * ...
-		 * comandoN;
-		 * }
-		 * else
-		 * comandoZ;
-		 * 
-		 * 3) if (condicao)
-		 * comando1;
-		 * else{
-		 * comando2;
-		 * comando3;
-		 * ...
-		 * comandoN;
-		 * }
-		 * 
-		 * 4) if (condicao){
-		 * comando1;
-		 * comando2;
-		 * ...
-		 * comandoN;
-		 * }
-		 * else{
-		 * comandoP;
-		 * comandoQ;
-		 * comandoZ;
-		 * }
-		 * 
-		 * AritmÈticos:
-		 *"+"
-		 *"-"
-		 *"*"
-		 *"/"
-		 *"%"
-		 *
-		 *Relacionais:
-		 *"=="
-		 *">"
-		 *">="
-		 *"<"
-		 *"<="
-		 *"!="
-		 *
-		 *Logicos:
-		 *&& -> e (and)
-		 *|| -> ou (or)
-		 *! -> Nao (not)
-		 */
-		
-		/*Ex.1(06/05/2022):
-		 * FaÁa um programa que receba como argumentos o sal·rio de uma pessoa, calcule e exiba o valor do desconto do imposto de renda.
-		 * O desconto do imposto de renda È calculado da seguinte forma:
-		 * Sal·rio < R$2000,00, o desconto do imposto de renda È 0
-		 * Se o sal·rio for >= R$2000,00 e < R$4000,00, desconto de 7,5% do sal·rio
-		 * Sal·rio for >= R$4000,00 e < R$7000,00, o desconto È de 15% do sal·rio.
-		 * Sal·rio for >= R$7000,00, descconto ser· de 27,5% do sal·rio. */
+/* Condicionais:
+* 
+* Condicional Simples:
+* 
+* Forma geral:
+* 
+* 1) if (condicao)
+*     comando;
+*     
+* 2) if(condicao){
+* comando1;
+* comando2;
+* ...
+* comandoN;
+* }
+* 
+* Condicional Composta:
+* 
+* Forma Geral:
+* 
+* 1) if (condicao)
+* comando1;
+* else
+* comando2;
+* 
+* 2) if (condicao){
+* comando1;
+* comando2;
+* ...
+* comandoN;
+* }
+* else
+* comandoZ;
+* 
+* 3) if (condicao)
+* comando1;
+* else{
+* comando2;
+* comando3;
+* ...
+* comandoN;
+* }
+* 
+* 4) if (condicao){
+* comando1;
+* comando2;
+* ...
+* comandoN;
+* }
+* else{
+* comandoP;
+* comandoQ;
+* comandoZ;
+* }
+* 
+* Aritm√©ticos:
+*"+"
+*"-"
+*"*"
+*"/"
+*"%"
+*
+*Relacionais:
+*"=="
+*">"
+*">="
+*"<"
+*"<="
+*"!="
+*
+*Logicos:
+*&& -> e (and)
+*|| -> ou (or)
+*! -> Nao (not)
+*/
 
-	public class MainClass {
-		public static void main(String[] args){
-				
-				double salario = Double.parseDouble (args[0]);
-				double ir;
-				if(salario < 2000)
-					ir = 0;
+/*Ex.1(06/05/2022):
+* Fa√ßa um programa que receba como argumentos o sal√°rio de uma pessoa, calcule e exiba o valor do desconto do imposto de renda.
+* O desconto do imposto de renda √© calculado da seguinte forma:
+* Sal√°rio < R$2000,00, o desconto do imposto de renda √© 0
+* Se o sal√°rio for >= R$2000,00 e < R$4000,00, desconto de 7,5% do sal√°rio
+* Sal√°rio for >= R$4000,00 e < R$7000,00, o desconto √© de 15% do sal√°rio.
+* Sal√°rio for >= R$7000,00, descconto ser√° de 27,5% do sal√°rio. */
+
+public class MainClass {
+	public static void main(String[] args){
+
+		double salario = Double.parseDouble (args[0]);
+		double ir;
+		if(salario < 2000)
+			ir = 0;
+		else
+			if(salario < 4000)
+				ir = salario * 0.075;
+			else
+				if(salario < 7000)
+					ir = salario * 0.15;
 				else
-					if(salario < 4000)
-						ir = salario * 0.075;
-					else
-						if(salario < 7000)
-							ir = salario * 0.15;
-						else
-							ir = salario * 0.275;
-				System.out.println("\nDesconto do IR: " + ir);
-
+					ir = salario * 0.275;
+		System.out.println("\nDesconto do IR: " + ir);
 			}
 
-	}
+}
 
 
 

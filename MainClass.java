538574080ddfa83eb4f9e2e1cb306037823c2b84
Eric Castellar <86270246+Ericcastell@users.noreply.github.com>
2022-05-06 10:45:24 -0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,103 @@
+public class MainClass {
+	public static void main(String[] args){
+		
+		/* Condicionais:
+		 * 
+		 * Condicional Simples:
+		 * 
+		 * Forma geral:
+		 * 
+		 * 1) if (condicao)
+		 *     comando;
+		 *     
+		 * 2) if(condicao){
+		 * comando1;
+		 * comando2;
+		 * ...
+		 * comandoN;
+		 * }
+		 * 
+		 * Condicional Composta:
+		 * 
+		 * Forma Geral:
+		 * 
+		 * 1) if (condicao)
+		 * comando1;
+		 * else
+		 * comando2;
+		 * 
+		 * 2) if (condicao){
+		 * comando1;
+		 * comando2;
+		 * ...
+		 * comandoN;
+		 * }
+		 * else
+		 * comandoZ;
+		 * 
+		 * 3) if (condicao)
+		 * comando1;
+		 * else{
+		 * comando2;
+		 * comando3;
+		 * ...
+		 * comandoN;
+		 * }
+		 * 
+		 * 4) if (condicao){
+		 * comando1;
+		 * comando2;
+		 * ...
+		 * comandoN;
+		 * }
+		 * else{
+		 * comandoP;
+		 * comandoQ;
+		 * comandoZ;
+		 * }
+		 * 
+		 * Aritméticos:
+		 *"+"
+		 *"-"
+		 *"*"
+		 *"/"
+		 *"%"
+		 *
+		 *Relacionais:
+		 *"=="
+		 *">"
+		 *">="
+		 *"<"
+		 *"<="
+		 *"!="
+		 *
+		 *Logicos:
+		 *&& -> e (and)
+		 *|| -> ou (or)
+		 *! -> Nao (not)
+		 */
+		
+		/*Ex.1(06/05/2022):
+		 * Faça um programa que receba como argumentos o salário de uma pessoa, calcule e exiba o valor do desconto do imposto de renda.
+		 * O desconto do imposto de renda é calculado da seguinte forma:
+		 * Salário < R$2000,00, o desconto do imposto de renda é 0
+		 * Se o salário for >= R$2000,00 e < R$4000,00, desconto de 7,5% do salário
+		 * Salário for >= R$4000,00 e < R$7000,00, o desconto é de 15% do salário.
+		 * Salário for >= R$7000,00, descconto será de 27,5% do salário. */
+		
+		double salario = Double.parseDouble(args[0]);
+		System.out.print(salario);
+		if (salario < 2000)
+			System.out.println("O Desconto do salario foi de R$0");
+		else if (salario < 4000)
+			System.out.println("O desconto do salario foi de R$"  +  (salario * 0.075));
+		else if (salario >= 4000 && salario <= 7000)
+			System.out.println("O desconto do salario foi de R$" + (salario * 0.15));
+		else if (salario >= 7000)
+			System.out.println("O desconto do salario foi de R$" + (salario * 0.275));
+		}
+	}
+
+
+
+
